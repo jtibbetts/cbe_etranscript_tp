ce747eb755dc512d3eed4fe776eccfeab3875a34
John Tibbetts <john.tibbetts@kinexis.com>
2015-06-03 10:27:35 -0700

    mod registration view

--- config/initializers/tool_provider.rb
+++ config/initializers/tool_provider.rb
@@ -1,2 +1,17 @@
 
-# For app init only
+include Lti2Commons
+include WireLogSupport
+
+NONCE_TIME_TO_LIVE = 300  # seconds
+Rails.application.config.nonce_cache = Cache.new :ttl => NONCE_TIME_TO_LIVE
+
+if ActiveRecord::Base.connection.table_exists? 'lti2_tp_registries'
+  Rails.application.config.tool_provider_registry = Lti2Tp::ToolProviderRegistry.new
+end
+
+wirelog_filename = Rails.application.config.tool_provider_registry.registry['wirelog_filename']
+if wirelog_filename.present?
+  Rails.application.config.wire_log = WireLog.new "ToolConsumer", File.expand_path(wirelog_filename)
+else
+  Rails.application.config.wire_log = nil
+end
