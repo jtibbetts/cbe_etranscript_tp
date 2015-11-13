e7a9f7e4c1b6d3ee4fe496935f4c74069ad3f8f1
Paul Gray <pfbgray@gmail.com>
2015-11-13 11:39:48 -0500

    Adding Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Starts the mysql database & runs the rails app.
+#
+
+
+#Setup the database
+service mysql start
+mysql --user="cbeuser" --password="cbepswd" --execute="CREATE DATABASE cbe_etranscript_tp;"
+mysql --user="cbeuser" --password="cbepswd" cbe_etranscript_tp < ./data/lti2_tc_mysql_init.sql
+mysql --user="cbeuser" --password="cbepswd" cbe_etranscript_tp < ./data/tpsampleapp.sql
+
+#Migrate the database
+rake db:migrate RAILS_ENV=development
+
+#run the app
+rails s -p 4000
