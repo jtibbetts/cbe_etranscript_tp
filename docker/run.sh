d90c4780bbaae15d94ff2334323f27d53421de9b
Paul Gray <pfbgray@gmail.com>
2015-11-19 11:57:13 -0500

    Adding tool_proxy_guid column to lti_tp_registrations

--- docker/run.sh
+++ docker/run.sh
@@ -7,7 +7,7 @@
 #Setup the database
 service mysql start
 mysql --user="cbeuser" --password="cbepswd" --execute="CREATE DATABASE cbe_etranscript_tp;"
-mysql --user="cbeuser" --password="cbepswd" cbe_etranscript_tp < ./data/lti2_tc_mysql_init.sql
+mysql --user="cbeuser" --password="cbepswd" cbe_etranscript_tp < ./data/lti2_tp_mysql_init.sql
 mysql --user="cbeuser" --password="cbepswd" cbe_etranscript_tp < ./data/tpsampleapp.sql
 
 #Migrate the database
